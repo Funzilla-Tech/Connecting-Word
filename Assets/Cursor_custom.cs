d7d148162ab9ddda6a12980a11c36799bd0f9eea
thanhday <maildithidaihoc@gmail.com>
2022-02-10 09:00:34 +0700

    fix cursor

---
+++ Assets/Cursor_custom.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor_custom : MonoBehaviour
+{
+    private Canvas parentCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentCanvas = GameObject.FindWithTag("MainCanvas").GetComponent<Canvas>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = GetMousePosition();
+      //  transform.position = new Vector3(transform.position.x, transform.position.y, -11);
+    }
+    internal Vector3 GetMousePosition() {
+        Vector2 movePos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            parentCanvas.transform as RectTransform,
+            Input.mousePosition, parentCanvas.worldCamera,
+            out movePos);
+        Vector3 positionToReturn = parentCanvas.transform.TransformPoint(movePos);
+        positionToReturn.z = parentCanvas.transform.position.z - 0.1f;
+        return positionToReturn;
+    }
+    
+}
